266a30d7039012fb1493b2816043e3c6135ea636
MADHANRAJ B <100281409+madhanraj45@users.noreply.github.com>
2026-09-04 22:37:16 +0530

    Update main.c

--- Node2_Door_Control/main.c
+++ Node2_Door_Control/main.c
@@ -1,1 +1,29 @@
+#include<LPC21XX.H>
+#include "delay.h"
+#include "lcdheader.h"
+#include "canstruct.h"
+#include "canheader.h"
+#define MOTOR1 (1 << 16)
+#define MOTOR2 (1 << 17)
+
+int main(){
+	CAN2_MSG RX;
+	IODIR0 |= MOTOR1 | MOTOR2;
+	IOCLR0 = MOTOR1 | MOTOR2;
+	
+	LCD_INIT();
+	CAN2_INIT();
+	LCD_COMMAND(0X80);
+	
+	while(1){
+		CAN2_RX(&RX);
+		if((RX.ID == 0X01) && (RX.RTR == 0) && ((RX.BYTEA & 0XFF) == 0Xaa)){
+			LCD_COMMAND(0X01);
+			LCD_STR("MOTOR ON");
+			
+			IOSET0 = MOTOR1;
+			IOCLR0 = MOTOR2;
+		}	
+	}
+}
 
